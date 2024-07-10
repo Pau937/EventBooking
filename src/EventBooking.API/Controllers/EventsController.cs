c0a48ab5fcd85ff83be79cb40c2bfabf06f72151
Paweł Kolbiarz <pawel.kolbiarz@gmail.com>
2024-07-10 18:02:22 +0200

    moved to src folder, events controller

---
+++ src/EventBooking.API/Controllers/EventsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventBooking.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
