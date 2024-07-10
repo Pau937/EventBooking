4d701355f1b696eb180aafedb0c1a8d88d3f3e00
Paweł Kolbiarz <pawel.kolbiarz@gmail.com>
2024-07-10 18:18:41 +0200

    added event domain model and projects

---
+++ src/EventBooking.Domain/Interfaces/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace EventBooking.Domain.Interfaces
+{
+    public abstract class Entity
+    {
+        protected Entity(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; protected set; }
+    }
+}
---
+++ src/EventBooking.Domain/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using EventBooking.Domain.Interfaces;
+
+namespace EventBooking.Domain.Models
+{
+    public sealed class Event : Entity
+    {
+        public Event(int id, string name, string country, string description, DateTime startDay, int numberOfSeats) : base(id)
+        {
+            Name = name;
+            Country = country;
+            Description = description;
+            StartDay = startDay;
+            NumberOfSeats = numberOfSeats;
+        }
+
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+        public string Description { get; private set; }
+        public DateTime StartDay { get; private set; }
+        public int NumberOfSeats { get; private set; }
+    }
+}
